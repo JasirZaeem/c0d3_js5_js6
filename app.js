162c7e16c1e8db3c02e9e8dcd6f6e15fe937c9b4
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2021-03-13 11:44:26 +0530

    Test Fix

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const js5_1_router = require("./src/js5/1");
 
+app.get("/", (req, res) => res.send("<h1>Hello</h1>"));
 app.use("/js5/1", js5_1_router);
 
 module.exports = app;
