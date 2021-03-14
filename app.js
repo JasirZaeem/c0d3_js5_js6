0d4cb9f76ddbcb3752b6743a6f7c02a184384807
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2021-03-14 20:00:44 +0530

    c0d3 challenge 7

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const js5_3_router = require("./src/js5/3");
 const js5_4_router = require("./src/js5/4");
 const js5_5_router = require("./src/js5/5");
 const js5_6_router = require("./src/js5/6");
+const js5_7_router = require("./src/js5/7");
 
 app.get("/", (req, res) => res.send("<h1>Hello</h1>"));
 app.use("/js5/1", js5_1_router);
@@ -17,5 +18,6 @@ app.use("/js5/3", js5_3_router);
 app.use("/js5/4", js5_4_router);
 app.use("/js5/5", js5_5_router);
 app.use("/js5/6", js5_6_router);
+app.use("/js5/7", js5_7_router);
 
 module.exports = app;
---
+++ src/js5/7.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const multer = require("multer");
+const Tesseract = require("tesseract.js");
+const { v4: uuid } = require("uuid");
+const fs = require("fs").promises;
+
+const EXPIRATION_DURATION = 1000 * 60 * 5; // Milliseconds in 5 minutes
+const jobs = {};
+
+const router = express.Router();
+const upload = multer({ dest: "uploads" });
+
+const doJob = async (job) => {
+  try {
+    const {
+      data: { text },
+    } = await Tesseract.recognize(job.path, "eng");
+    job.text = text;
+    job.status = "completed";
+  } catch (e) {
+    job.status = "error";
+  } finally {
+    job.expireTimeout = setTimeout(async () => {
+      await fs.unlink(job.path);
+      delete jobs[job.id];
+    }, EXPIRATION_DURATION);
+  }
+};
+
+router.post("/files", upload.array("assets[]"), (req, res) => {
+  const jobsSpawned = req.files.map(({ path, originalname }) => {
+    const id = uuid();
+    const job = {
+      path,
+      filename: originalname,
+      status: "pending",
+      timestamp: Date.now(),
+    };
+    jobs[id] = job;
+    doJob(job);
+    return {
+      jobid: id,
+      filename: originalname,
+      url: `${req.hostname}/api/jobs/${id}`,
+    };
+  });
+  return res.status(202).json({ jobs: jobsSpawned });
+});
+
+router.get("/api/jobs/:id", express.json(), (req, res) => {
+  const { filename, status, text = "" } = jobs[req.params.id] || {};
+  if (!status) return res.status(404).json({ error: "Job not found" });
+  return res.json({ filename, status, text });
+});
+
+module.exports = router;
